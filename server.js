85d80be4a63e9396a3ec952d5c2c2a6ebe068a91
arsalanahemd <arslanahmed3529@gmail.com>
2026-04-20 11:36:48 +0500

    Refactor database connection import statements

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
+// const connectDB = require("./config/db");
+const connectDB =require("./config/db");
 const userRoutes = require ("./routes/userRoutes");
 const productsRoutes = require ("./routes/productsRoutes")
 const categoryRoutes = require("./routes/categoryRoutes");
