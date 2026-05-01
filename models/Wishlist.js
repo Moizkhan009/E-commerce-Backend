bf1c98ccede0f19ef5a37a8a549ccd97134a9501
arsalanahemd <arslanahmed3529@gmail.com>
2026-05-01 13:54:14 +0500

    add full cart and wishlist system with API integration

--- models/Wishlist.js
+++ models/Wishlist.js
@@ -1,31 +1,56 @@
+// const mongoose = require("mongoose");
+
+// const wishlistSchema = new mongoose.Schema(
+//   {
+//     users: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//       index: true,
+//     },
+
+//     product: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Product",
+//       required: true,
+//     },
+
+//     // snapshot (optional but pro)
+//     name: String,
+//     image: String,
+//     price: Number,
+//   },
+//   {
+//     timestamps: true,
+//   }
+// );
+
+// // 🔥 prevent duplicate (most important)
+// wishlistSchema.index({ user: 1, product: 1 }, { unique: true });
+
+// module.exports = mongoose.model("Wishlist", wishlistSchema);
 const mongoose = require("mongoose");
 
 const wishlistSchema = new mongoose.Schema(
   {
-    users: {
+    user: {                                    // 🔥 "user" (singular)
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
-      index: true,
     },
-
     product: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Product",
       required: true,
     },
-
-    // snapshot (optional but pro)
     name: String,
     image: String,
     price: Number,
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true }
 );
 
-// 🔥 prevent duplicate (most important)
+// duplicate prevent
 wishlistSchema.index({ user: 1, product: 1 }, { unique: true });
 
 module.exports = mongoose.model("Wishlist", wishlistSchema);
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/authMiddleware")
+const { getCart } = require("../controllers/cartController");
 
 const { addToCart } = require("../controllers/cartController");
+const { updateCartItem } = require("../controllers/cartController");
+const { removeFromCart } = require("../controllers/cartController");
 
 router.post("/", auth , addToCart);
+router.get("/", auth, getCart);
+router.put("/",    auth, updateCartItem); // PUT    /api/cart  ← YEH NAHI THA
+router.delete("/", auth, removeFromCart); // DELETE /api/cart  ← YEH NAHI THA
 
 module.exports = router;
